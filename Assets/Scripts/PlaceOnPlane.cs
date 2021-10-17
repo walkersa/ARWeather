071f31b5b31885297a897c8617a4eab4ca264f3d
walkersa <murraywalker.one@gmail.com>
2021-10-17 10:19:05 +0200

    updated config to metric call

--- Assets/Scripts/PlaceOnPlane.cs
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -21,7 +21,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
         GameObject m_PlacedPrefab;
 
         public float offset;
-        public TextMeshProUGUI debugText;
+        //public TextMeshProUGUI debugText;
 
         /// <summary>
         /// The prefab to instantiate on touch.
